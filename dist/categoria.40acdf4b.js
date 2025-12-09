0cbb160c6bfc142583e7a945ff5322d223125e95
Clara Salvador <clasalvill@gmail.com>
2025-12-10 00:13:10 +0100

    Añadida carpeta dist para Netlify

---
+++ dist/categoria.40acdf4b.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("portada1"),t=document.getElementById("portada2");e&&(e.src={}),t&&(t.src={})});
+//# sourceMappingURL=categoria.40acdf4b.js.map
